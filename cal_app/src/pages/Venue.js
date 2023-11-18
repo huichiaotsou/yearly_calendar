0b97e050589eefaf6a47da67c1bbd5a336742f4b
huichiaotsou <huichiao.tsou@gmail.com>
2023-11-18 12:03:50 +0800

    add venue related files, to try to add app context for venues and events as those are shared among whole app

--- cal_app/src/pages/Venue.js
+++ cal_app/src/pages/Venue.js
@@ -1,5 +1,14 @@
+import "../styles/venue.css";
+
+import Venues from "../components/venues";
+import { VenueProvider } from "../contexts/venue";
+
 function Venue() {
-  return <div></div>;
+  return (
+    <VenueProvider>
+      <Venues />
+    </VenueProvider>
+  );
 }
 
 export default Venue;
