d605148a4d6076ac89927226a2a1dc46bfda36d8
huichiaotsou <huichiao.tsou@gmail.com>
2023-11-18 16:14:19 +0800

    to merge cal planning context with app context

--- cal_app/src/pages/Venue.js
+++ cal_app/src/pages/Venue.js
@@ -1,14 +1,9 @@
 import "../styles/venue.css";
 
 import Venues from "../components/venues";
-import { VenueProvider } from "../contexts/venue";
 
 function Venue() {
-  return (
-    <VenueProvider>
-      <Venues />
-    </VenueProvider>
-  );
+  return <Venues />;
 }
 
 export default Venue;
