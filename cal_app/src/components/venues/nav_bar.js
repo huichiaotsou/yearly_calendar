fb6fbc0e239d69e4d03b61c20caa89fa74c86b0e
huichiaotsou <huichiao.tsou@gmail.com>
2023-11-23 17:48:52 +0800

    add new loc

--- cal_app/src/components/venues/nav_bar.js
+++ cal_app/src/components/venues/nav_bar.js
@@ -14,31 +14,38 @@ function NavBarCity({ checkedLocation, city, handleOnclickTab }) {
 export default function NavBar({ checkedLocation, setCheckedLocation }) {
   const { locations } = useAppContext();
 
+  console.log("locations: ", locations);
+
   function handleOnclickTab(tabName) {
     setCheckedLocation(tabName);
   }
 
   return (
-    <div className="tabs">
-      <ul>
-        <li
-          className={checkedLocation === "All" ? "is-active" : ""}
-          onClick={() => handleOnclickTab("All")}
-        >
-          <a>All</a>
-        </li>
+    <>
+      <div className="tabs">
+        <ul>
+          <li
+            className={checkedLocation === "All" ? "is-active" : ""}
+            onClick={() => handleOnclickTab("All")}
+          >
+            <a>All</a>
+          </li>
 
-        {locations.map((city) => {
-          return (
-            <NavBarCity
-              key={city}
-              checkedLocation={checkedLocation}
-              city={city}
-              handleOnclickTab={handleOnclickTab}
-            />
-          );
-        })}
-      </ul>
-    </div>
+          {locations.map((city) => {
+            return (
+              <NavBarCity
+                key={city}
+                checkedLocation={checkedLocation}
+                city={city}
+                handleOnclickTab={handleOnclickTab}
+              />
+            );
+          })}
+          <li id="add_venue_button" className="button">
+            Add Venue
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
